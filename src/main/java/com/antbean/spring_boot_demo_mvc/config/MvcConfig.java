feca32bb721833b861342e4da995db3c454e151e
lengyucc <1902328305@qq.com>
2017-04-26 23:21:01 +0800

    web

---
+++ src/main/java/com/antbean/spring_boot_demo_mvc/config/MvcConfig.java
@@ -0,0 +1,20 @@
+package com.antbean.spring_boot_demo_mvc.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.antbean.spring_boot_demo_mvc")
+public class MvcConfig {
+	public InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/classes/views/");
+		viewResolver.setSuffix(".jsp");
+		viewResolver.setViewClass(JstlView.class);
+		return viewResolver;
+	}
+}
